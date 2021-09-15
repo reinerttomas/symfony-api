51b1f1df3e1ec960de8b24fa215209010717b24c
Tomáš Reinert <reinerttomas@gmail.com>
2021-09-15 20:39:06 +0200

    post

---
+++ src/Entity/Post.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use App\Utils\DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $author;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTime $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTime $updatedAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTime $publishedAt;
+    
+    public function __construct(
+        string $title,
+        string $author,
+        string $content,
+    ) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->content = $content;
+        $this->createdAt = new DateTime();
+        $this->updatedAt = null;
+        $this->publishedAt = null;
+    }
+    
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function changeTitle(string $title): Post
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+    
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function changeAuthor(string $author): Post
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+    
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function changeContent(string $content): Post
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+    
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+    
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function updated(): Post
+    {
+        $this->updatedAt = new DateTime();
+
+        return $this;
+    }
+    
+    public function getPublishedAt(): ?DateTime
+    {
+        return $this->publishedAt;
+    }
+
+    public function publish(): Post
+    {
+        $this->publishedAt = new DateTime();
+
+        return $this;
+    }
+}
