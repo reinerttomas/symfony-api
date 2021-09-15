018ef41336ac9d234ae57f3948825b26f18b8cbb
Tomáš Reinert <reinerttomas@gmail.com>
2021-09-15 20:55:06 +0200

    phpstan

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -7,10 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Post|null find($id, $lockMode = null, $lockVersion = null)
- * @method Post|null findOneBy(array $criteria, array $orderBy = null)
- * @method Post[]    findAll()
- * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @extends ServiceEntityRepository<Post>
  */
 class PostRepository extends ServiceEntityRepository
 {
